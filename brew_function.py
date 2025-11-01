e446774f6646119be01ab4c852ee9713b890d879
Joey Castaneda <joeyscastaneda@gmail.com>
2025-11-01 14:18:31 -0700

    Create function environment class, such that we can allow lexical scoping to function properly

--- brew_function.py
+++ brew_function.py
@@ -1,7 +1,31 @@
 from element import Element
 
 
-def get_func_list(ast):
-    #Get function list from the program at head of ast
-    func_nodes = ast.dict['functions']
-    return func_nodes
+class FunctionEnv():
+    def __init__(self, func_name):
+        self.func_name = func_name
+        self.funcscope = {}
+
+    def insert_varname(self, varname):
+        if self.variable_exists(varname):
+            return False
+        self.funcscope[varname] = None
+        return True
+    def variable_exists(self, varname):
+        return varname in self.funcscope
+    def variable_assigned(self,varname):
+        if self.funcscope[varname] == None or self.variable_exists(varname) == False:
+            return False
+        else:
+            return True
+    def assign_variable(self, varname, value):
+        if self.variable_exists(varname) == False:
+            return False
+            #HAVE TO CALL ERROR ON FALSE RETURN
+        else:
+            self.funcscope[varname] = value
+            return True
+    def get_func_name(self):
+        return self.func_name
+    
+    
